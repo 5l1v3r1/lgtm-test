68a1229b0e09efbdfb0ca11384c55dccfa37c0d5
Paulino Calderon <cldrn@users.noreply.github.com>
2019-10-15 10:25:34 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,17 @@
 int main(int   argc, char *argv[])
 {
   char certificado[10];
-
+  char comando[10];
+	
 	if (argc > 3) {
 		if (sprintf(certificado, "%s", argv[3]) < 0) {
 			//oops
 		}
 		printf("\n\n[%s]\n\n", certificado);
 		int a = atoi(argv[2]);
+		if (sprintf(comando, "/usr/spei/scripts/check_mysql %s", argv[0]) < 0) {
+			//?
+		}
+		int ret_val = system(comando);
   }
 }
